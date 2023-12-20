57c8daadb062670f71c4cb609c1b28f09dcd9389
Stuart Cook <stuart_cook@waters.com>
2023-12-19 21:48:38 -0700

    tests: datetime json serializer

--- tests/test_class_generator_common_utils.py
+++ tests/test_class_generator_common_utils.py
@@ -1,5 +1,10 @@
 import pytest
-from tadatakit.class_generator.common_utils import pascal_to_snake, snake_to_pascal
+from tadatakit.class_generator.common_utils import (
+    pascal_to_snake,
+    snake_to_pascal,
+    json_serializer,
+)
+import datetime
 
 
 def test_pascal_to_snake():
@@ -16,6 +21,17 @@ def test_snake_to_pascal():
     assert snake_to_pascal("snake") == "Snake"
     assert snake_to_pascal("snake_case_example") == "SnakeCaseExample"
     assert snake_to_pascal("") == ""
+
+
+def test_json_serializer_datetime():
+    dt = datetime.datetime(2021, 1, 1, 12, 0)
+    expected = "2021-01-01T12:00:00.000000Z"
+    assert json_serializer(dt) == expected
+
+
+def test_json_serializer_unsupported_type():
+    with pytest.raises(TypeError):
+        json_serializer({"unsupported": "object"})
 
 
 if __name__ == "__main__":
