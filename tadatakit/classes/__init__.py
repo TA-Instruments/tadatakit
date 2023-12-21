33507c2d623742d0579c57e130ac75e43558eae5
Stuart Cook <stuart_cook@waters.com>
2023-12-21 16:35:50 -0700

    classes: step_dataframes added

--- tadatakit/classes/__init__.py
+++ tadatakit/classes/__init__.py
@@ -1,15 +1,18 @@
 import pandas as pd
-from typing import Dict
-from tadatakit.class_generator import Experiment
+from typing import Dict, Optional, List, Tuple
+from tadatakit.class_generator import Experiment, Procedure
 
 
-def create_dataframe(self: Experiment) -> pd.DataFrame:
+def create_dataframe(
+    self: Experiment, start_index: Optional[int] = None, end_index: Optional[int] = None
+) -> pd.DataFrame:
     """
     Create a pandas DataFrame from the results in an Experiment object.
 
-    The DataFrame is created using the results in the Experiment object,
-    with appropriate data types and column names based on the Experiment's
-    result column headers.
+    Args:
+        self (Experiment): The Experiment instance.
+        start_index (Optional[int]): The starting index for slicing the results. Defaults to None.
+        end_index (Optional[int]): The ending index for slicing the results. Defaults to None.
 
     Returns:
         pd.DataFrame: A DataFrame representation of the Experiment's results.
@@ -20,15 +23,65 @@ def create_dataframe(self: Experiment) -> pd.DataFrame:
     for col, details in self.results.column_headers.items():
         column_dtypes[col] = "float64" if details["ValueType"] == "Number" else "object"
         unit_name = details.get("Unit", {"Name": None})["Name"]
-        column_names[col] = f"{details['DisplayName']}"
+        column_names[col] = details["DisplayName"]
         if unit_name is not None:
             column_names[col] += f" / {unit_name}"
 
-    return (
-        pd.DataFrame(self.results.rows)
-        .astype(column_dtypes)
-        .rename(columns=column_names)
-    )
+    df = pd.DataFrame(self.results.rows[start_index:end_index])
+    df = df.astype({c: column_dtypes[c] for c in df.columns})
+    return df.rename(columns=column_names)
 
 
-setattr(Experiment, "dataframe", property(create_dataframe))
+def create_whole_dataframe(self: Experiment) -> pd.DataFrame:
+    """
+    Create a pandas DataFrame containing all results from the Experiment object.
+
+    Args:
+        self (Experiment): The Experiment instance.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing all results of the Experiment.
+    """
+    return create_dataframe(self, None, None)
+
+
+def get_step_indices(self: Procedure) -> List[Tuple[int, int]]:
+    """
+    Get the start and end indices for each step in a Procedure.
+
+    Args:
+        self (Procedure): The Procedure instance.
+
+    Returns:
+        List[Tuple[int, int]]: A list of tuples containing start and end indices for each step.
+    """
+    return [
+        (
+            step.results_mapping["StartIndex"],
+            step.results_mapping["StartIndex"] + step.results_mapping["Count"],
+        )
+        if step.results_mapping is not None
+        else None
+        for step in self.steps
+    ]
+
+
+def get_dataframes_by_step(self: Experiment) -> List[Optional[pd.DataFrame]]:
+    """
+    Create a list of pandas DataFrames, each representing the results of a step in the Experiment.
+
+    Args:
+        self (Experiment): The Experiment instance.
+
+    Returns:
+        List[Optional[pd.DataFrame]]: A list of DataFrames, one for each step in the Experiment's procedure.
+    """
+    return [
+        create_dataframe(self, indices[0], indices[1]) if indices is not None else None
+        for indices in self.procedure.step_indices
+    ]
+
+
+setattr(Experiment, "dataframe", property(create_whole_dataframe))
+setattr(Procedure, "step_indices", property(get_step_indices))
+setattr(Experiment, "step_dataframes", property(get_dataframes_by_step))
